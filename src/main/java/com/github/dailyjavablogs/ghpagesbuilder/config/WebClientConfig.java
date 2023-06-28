ca2f5ccd4e0ba8dca27fff6398c969d410790f06
Lukáš Vasek <bilaak@gmail.com>
2023-06-28 14:02:51 +0200

    Reconfigured client to handle bigger rss feeds

--- src/main/java/com/github/dailyjavablogs/ghpagesbuilder/config/WebClientConfig.java
+++ src/main/java/com/github/dailyjavablogs/ghpagesbuilder/config/WebClientConfig.java
@@ -3,9 +3,11 @@ package com.github.dailyjavablogs.ghpagesbuilder.config;
 import io.netty.handler.ssl.SslContext;
 import io.netty.handler.ssl.SslContextBuilder;
 import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
+import org.springframework.boot.autoconfigure.codec.CodecProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.netty.http.client.HttpClient;
 
@@ -15,7 +17,7 @@ import javax.net.ssl.SSLException;
 public class WebClientConfig {
 
 	@Bean
-	WebClient webClient() throws SSLException {
+	WebClient webClient(final CodecProperties codecProperties) throws SSLException {
 		final SslContext sslContext = SslContextBuilder
 			.forClient()
 			.trustManager(InsecureTrustManagerFactory.INSTANCE)
@@ -24,6 +26,14 @@ public class WebClientConfig {
 		final HttpClient httpClient = HttpClient.create().secure(t -> t.sslContext(sslContext));
 		return WebClient.builder()
 			.clientConnector(new ReactorClientHttpConnector(httpClient))
+			.exchangeStrategies(exchangeStrategies(codecProperties))
+			.build();
+	}
+
+	private ExchangeStrategies exchangeStrategies(final CodecProperties codecProperties) {
+		return ExchangeStrategies.builder()
+			.codecs(codecs -> codecs.defaultCodecs()
+				.maxInMemorySize(Long.valueOf(codecProperties.getMaxInMemorySize().toBytes()).intValue()))
 			.build();
 	}
 }
